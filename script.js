da06a8edd112d0dc3f75a1311b6f24121cf546bd
O.o нα∂ιαѕιяι o.O <78082625+hadiasiri@users.noreply.github.com>
2024-06-18 03:46:10 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,25 +1,11 @@
 
-document.addEventListener("DOMContentLoaded", function() {
-    var input = document.querySelector("#phoneNumber");
-    var iti = window.intlTelInput(input, {
-        initialCountry: "sa",
-        preferredCountries: ["sa", "ae", "kw", "qa", "om"],
-        separateDialCode: true,
-        utilsScript: "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js"
-    });
-
-    window.generateLink = function() {
-        var countryCode = document.getElementById("countryCode").value;
-        var phoneNumber = document.getElementById("phoneNumber").value;
-        var fullNumber = countryCode + phoneNumber;
-        var linkOutput = document.getElementById("linkOutput");
-
-        if (phoneNumber) {
-            var link = "https://wa.me/" + fullNumber;
-            linkOutput.innerHTML = "الرابط: <a href='" + link + "' target='_blank'>" + link + "</a>";
-        } else {
-            linkOutput.textContent = "يرجى إدخال رقم الهاتف.";
-        }
-    };
-});
-    
+function generateLink() {
+    const countryCode = document.getElementById('countryCode').value;
+    const phoneNumber = document.getElementById('phoneNumber').value.trim();
+    if(phoneNumber) {
+        const link = `https://wa.me/${countryCode}${phoneNumber}`;
+        document.getElementById('link').innerHTML = `<a href="${link}" target="_blank">${link}</a>`;
+    } else {
+        document.getElementById('link').innerHTML = 'الرجاء إدخال رقم هاتف صالح';
+    }
+}
